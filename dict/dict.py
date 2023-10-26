15d97f670041b09206b9075f237c5345fec481ec
Karim2004Marolix <karim2004.marolix@gmail.com>
2023-10-26 17:44:24 +0530

    dict task

---
+++ dict/dict.py
@@ -0,0 +1,22 @@
+emp_dict = {}
+emp_dict["Name"] = input("enter name: ")
+emp_dict["domain"] = input("enter domain: ")
+emp_dict["empld"] = input("enter id: ")
+emp_dict["email"] = input("enter email: ")
+print(emp_dict)
+
+
+item_dict = {}
+item_dict["item name"] = input("enter item name: ")
+item_dict["item price"] = input("enter item price: ")
+item_dict["no of items"] = input("enter items count: ")
+print(item_dict)
+
+
+village_dict = {}
+village_dict["aadhaar number"] = input("enter aadhaar number: ")
+village_dict["head of the house"] = input("enter head of the house name: ")
+village_dict["door number"] = input("enter door number: ")
+village_dict["street name"] = input("enter street: ")
+village_dict["villeage name"] = input("enter village: ")
+print(item_dict)
